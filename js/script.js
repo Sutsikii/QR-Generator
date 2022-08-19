aaebdc96f75e52aaa746cf8e6ab4e7aa69029c01
Sutsiki <nicolas-dutertre@bbox.fr>
2022-08-19 16:20:27 +0200

    Bouton de téléchargement

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
+// Générateur de QR Code 
+
 const wrapper = document.querySelector(".wrapper"),
 generateBtn = wrapper.querySelector(".form button"),
 qrInput = wrapper.querySelector(".form input"),
-qrImg = wrapper.querySelector(".qr-code img");
+qrImg = wrapper.querySelector(".qr-code img"),
+downloadBtn = wrapper.querySelector(".download-btn");
 
 generateBtn.addEventListener("click", () => 
 {
@@ -11,7 +14,30 @@ generateBtn.addEventListener("click", () =>
     qrImg.addEventListener("load", () =>
     {
         wrapper.classList.add("active");
+        downloadBtn.classList.add("active")
     });
     
 });
 
+downloadBtn.addEventListener("click", e => 
+{
+    e.preventDefault();
+    fetchFile(qrImg.src);
+    
+});
+
+function fetchFile(url)
+{
+
+    fetch(url).then(res => res.blob()).then(file => {
+        let qrValue = qrInput.value;
+        let tempUrl = URL.createObjectURL(file);
+        let aTag = document.createElement("a");
+        aTag.href = tempUrl;
+        aTag.download = `QR-CODE-${qrValue}`;
+        document.body.appendChild(aTag);
+        aTag.click();
+        aTag.remove();
+    })
+
+};
